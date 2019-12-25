0f3b4965a830b399c7c5d54ee00defddc29dd77d
sonali mahavar <53106053+sonali-mahavar@users.noreply.github.com>
2019-12-25 10:55:09 +0530

    Add files via upload

---
+++ rectangle.java.java
@@ -0,0 +1,16 @@
+
+
+package javaapplication2;
+import java.util.Scanner;
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+       System.out.print("ENTER LENGTH AND BREADTH"); 
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int c=a+b;
+       System.out.println(c);// TODO code application logic here
+    }
+    
+}
